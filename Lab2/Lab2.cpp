f0cba35bbded5443d1ef14b07c179634bfcb3db5
KBS <matiy@naver.com>
2019-01-17 14:20:44 +0900

    Lab2_5submit
    Lab2_5submit

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -1,58 +1,58 @@
 ﻿#include <iomanip>
+#include <string>
 #include "Lab2.h"
-constexpr int OCT_LENGTH = 12;
-constexpr int DEC_LENGTH = 10;
-constexpr int HEX_LENGTH = 8;
-constexpr int POS_LENGTH = 5;
-constexpr int NUM_LENGTH = 15;
+
 namespace lab2
 {
 	void PrintIntegers(std::istream& in, std::ostream& out)
 	{
-		out << std::setw(OCT_LENGTH) << "oct" << " " << std::setw(DEC_LENGTH) << "dec" << " " << std::setw(HEX_LENGTH) << "hex" << std::endl;
-		out << std::setfill('-') << std::setw(OCT_LENGTH) << "" << " " << std::setw(DEC_LENGTH) << "" << " " << std::setw(HEX_LENGTH) << "" << std::endl << std::setfill(' ');
-
+		constexpr int octLength = 12;
+		constexpr int decLength = 10;
+		constexpr int hecLength = 8;
+		out << std::setw(octLength) << "oct" << " " << std::setw(decLength) << "dec" << " " << std::setw(hecLength) << "hex" << std::endl;
+		out << std::setfill('-') << std::setw(octLength) << "" << " " << std::setw(decLength) << "" << " " << std::setw(hecLength) << "" << std::endl << std::setfill(' ');
+		int num;
+		std::string trash;
 		while (!in.eof())
 		{
-
-			int num;
 			in >> num;
 			if (in.fail())
 			{
 				in.clear();
-				in.ignore(LLONG_MAX, ' ');
-
-				continue;
+				in >> trash;
 			}
-			if (num < 0)
+			else
 			{
-				continue;
+				out << std::setw(octLength) << std::oct << num << " "
+					<< std::setw(decLength) << std::dec << num << " "
+					<< std::setw(hecLength) << std::uppercase << std::hex << num << std::endl;
 			}
-			out << std::setw(OCT_LENGTH) << std::oct << num << " "
-				<< std::setw(DEC_LENGTH) << std::dec << num << " "
-				<< std::setw(HEX_LENGTH) << std::uppercase << std::hex << num << std::endl;
 		}
 	}
 
 	void PrintMaxFloat(std::istream& in, std::ostream& out)
 	{
-		float max = FLT_MIN;
+		constexpr int posLength = 5;
+		constexpr int numLength = 15;
 		out << std::showpos << std::showpoint << std::internal << std::fixed << std::setprecision(3);
+		float max = -FLT_MAX;
+		float num;
+		std::string trash;
 		while (!in.eof())
 		{
-			float num;
 			in >> num;
 			if (in.fail())
 			{
 				in.clear();
-				in.ignore(LLONG_MAX, ' ');
-
-				continue;
+				in >> trash;
 			}
-			out << std::setw(POS_LENGTH) << "" << std::setw(NUM_LENGTH) << num << std::endl;
-			if (num > max)
-				max = num;
+			else
+			{
+				out << std::setw(posLength) << "" << std::setw(numLength) << num << std::endl;
+				if (num > max)
+					max = num;
+			}
 		}
-		out << std::setw(POS_LENGTH) << "max:" << std::setw(NUM_LENGTH) << max << std::endl;
+		out << std::setw(posLength) << std::left << "max:" << std::setw(numLength) << max << std::endl;
 	}
 }
