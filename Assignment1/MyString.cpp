8941535c6ae491f81ed6b028a812c5cff94a2acd
KBS <matiy@naver.com>
2019-01-25 11:27:59 +0900

    Assignment1_2submit
    Assignment1_2submit

--- Assignment1/MyString.cpp
+++ Assignment1/MyString.cpp
@@ -26,55 +26,57 @@ namespace assignment1
 
 	unsigned int MyString::GetLength() const
 	{
-		return mStringLen - 1;
+		//return mStringLen - 1;
 	}
 
 	const char* MyString::GetCString() const
 	{
-		return mString;
+		//return mString;
 	}
 
 	void MyString::Append(const char* s)
 	{
-		int stringLen = mStringLen + StrLen(s);
-		char* stringSum = new char[stringLen];
-		MemCpy(stringSum, mString, mStringLen - 1);
-		MemCpy(stringSum + (mStringLen - 1), s, StrLen(s) + 1);
-		delete[] mString;
-		mString = stringSum;
-		mStringLen = stringLen;
+		//int stringLen = mStringLen + StrLen(s);
+		//char* stringSum = new char[stringLen];
+		//MemCpy(stringSum, mString, mStringLen - 1);
+		//MemCpy(stringSum + (mStringLen - 1), s, StrLen(s) + 1);
+		//delete[] mString;
+		//mString = stringSum;
+		//mStringLen = stringLen;
 	}
 
 	MyString MyString::operator+(const MyString& other) const
 	{
-		MyString sum("");
-		sum.Append(mString);
-		sum.Append(other.mString);
-		return sum;
+		//MyString sum("");
+		//sum.Append(mString);
+		//sum.Append(other.mString);
+		//return sum;
 	}
 
 	int MyString::IndexOf(const char* s)
 	{
-		int index = 0;
-		bool bSearch = false;
-		int stringLen = StrLen(s);
-		while (mString[index] != '\0')
-		{
-			for (int count = 0; count < stringLen; count++)
-			{
-				if (mString[index] == s[count])
-				{
-					bSearch = true;
-					break;
-				}
-			}
-			if (bSearch)
-				break;
-			index++;
-		}
-		if (!bSearch)
-			index = -1;
-		return index;
+		//int index = 0;
+		//if (s == "")
+		//	return index;
+		//bool bSearch = false;
+		//int stringLen = StrLen(s);
+		//while (mString[index] != '\0')
+		//{
+		//	for (int count = 0; count < stringLen; count++)
+		//	{
+		//		if (mString[index] == s[count])
+		//		{
+		//			bSearch = true;
+		//			break;
+		//		}
+		//	}
+		//	if (bSearch)
+		//		break;
+		//	index++;
+		//}
+		//if (!bSearch)
+		//	index = -1;
+		//return index;
 	}
 
 	int MyString::LastIndexOf(const char* s)
@@ -88,22 +90,22 @@ namespace assignment1
 
 	bool MyString::RemoveAt(unsigned int index)
 	{
-		if (mStringLen <= index)
-			return false;
-
-		while (mString[index + 1]!= '\0')
-		{
-			mString[index] = mString[index + 1];
-			index++;
-		}
-		mString[index] = '\0';
-		int stringLen = StrLen(mString) + 1;
-		char* changeString = new char[stringLen];
-		MemCpy(changeString, mString, stringLen);
-		delete[] mString;
-		mString = changeString;
-		mStringLen = stringLen;
-		return true;
+		//if (mStringLen <= index)
+		//	return false;
+		//
+		//while (mString[index + 1] != '\0')
+		//{
+		//	mString[index] = mString[index + 1];
+		//	index++;
+		//}
+		//mString[index] = '\0';
+		//int stringLen = StrLen(mString) + 1;
+		//char* changeString = new char[stringLen];
+		//MemCpy(changeString, mString, stringLen);
+		//delete[] mString;
+		//mString = changeString;
+		//mStringLen = stringLen;
+		//return true;
 	}
 
 	void MyString::PadLeft(unsigned int totalLength)
