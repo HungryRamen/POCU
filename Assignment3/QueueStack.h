f80c0d1801fc712f60f38c01c99af08385dd2e4c
KBS <matiy@naver.com>
2019-03-10 00:20:16 +0900

    Assignment3_38submit
    Assignment3_38submit

--- Assignment3/QueueStack.h
+++ Assignment3/QueueStack.h
@@ -141,20 +141,7 @@ namespace assignment3
 	template<typename T>
 	inline double QueueStack<T>::Average()
 	{
-		double value = 0;
-		if (mQueueStack.size() == 1)
-		{
-			value = mQueueStack.front().size();
-		}
-		else if (mQueueStack.size() == 2)
-		{
-			value = mQueueStack.front().size() + mQueueStack.back().size();
-		}
-		else
-		{
-			value = mQueueStack.front().size() + mQueueStack.back().size() + (mQueueStack.size() - 2) * mMaxStackSize;
-		}
-		return floor(mQueueStackSum / value * pow(10.0, 3) + 0.5) / pow(10.0, 3);
+		return floor(mQueueStackSum / static_cast<double>(Count()) * pow(10.0, 3) + 0.5) / pow(10.0, 3);
 	}
 	template<typename T>
 	inline T QueueStack<T>::Sum()
